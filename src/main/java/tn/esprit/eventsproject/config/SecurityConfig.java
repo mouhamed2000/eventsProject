da70d602b9751cdcc6eb291d56786b7c699439fc
mouhamed2000 <125143491+mouhamed2000@users.noreply.github.com>
2024-12-10 02:33:37 +0100

    Update SecurityConfig.java

--- src/main/java/tn/esprit/eventsproject/config/SecurityConfig.java
+++ src/main/java/tn/esprit/eventsproject/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().permitAll()
-                );
+                )
+                .httpBasic(withDefaults());
+
         return http.build();
     }
 }
-
